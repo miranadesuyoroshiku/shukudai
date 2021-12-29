93d8e831057deb8454eb6db5ac1bdfa71d7fa1be
miranadesu <80235777+miranadesuyoroshiku@users.noreply.github.com>
2021-12-28 18:06:43 -0800

    I think this works!

--- shukudai.cpp
+++ shukudai.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void printGameBoard(char game_board[3][3]);
-void checkGameStatus(char game_board[3][3]);
+void checkGameStatus(char game_board[3][3], int step);
 
 int main()
 {
@@ -49,7 +49,7 @@ int main()
 			}
 		}
 		printGameBoard(game_board);
-		checkGameStatus(game_board);
+		checkGameStatus(game_board, step);
 		step++;
 	} while (step < 9);
 
@@ -67,7 +67,54 @@ void printGameBoard(char game_board[3][3])
 	cout << "*---*---*---*" << endl;
 }
 
-void checkGameStatus(char game_board[3][3])
+void checkGameStatus(char game_board[3][3], int step)
 {
-
+	bool game_status = false;
+	char winner_code;
+	for (int i = 0; i < 3; i++)
+	{
+		if (game_board[i][0] == game_board[i][1] && game_board[i][1] == game_board[i][2] && game_board[i][0] != 'n')
+		{
+			game_status = true;
+			winner_code = game_board[i][0];
+		}
+		else if (game_board[0][i] == game_board[1][i] && game_board[1][i] == game_board[2][i] && game_board[0][i] != 'n')
+		{
+			game_status = true;
+			winner_code = game_board[0][i];
+		}
+		else if (game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2] && game_board[0][0] != 'n')
+		{
+			game_status = true;
+			winner_code = game_board[0][0];
+		}
+		else if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0] && game_board[0][2] != 'n')
+		{
+			game_status = true;
+			winner_code = game_board[0][2];
+		}
+		else
+		{
+			game_status = false;
+		}
+		if (game_status == true)
+		{
+			break;
+		}
+	}
+	if (game_status == true)
+	{
+		if (winner_code == 'X')
+		{
+			cout << "Player A wins!" << endl;
+		}
+		if (winner_code == 'O')
+		{
+			cout << "Player B wins!" << endl;
+		}
+	}
+	if (game_status == false && step == 8)
+	{
+		cout << "Draw" << endl;
+	}
 }
